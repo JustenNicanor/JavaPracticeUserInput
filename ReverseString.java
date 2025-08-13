d2ae7a0ca5f615bed513285bceacc58848618d47
Justeeen10 <92636904+JustenNicanor@users.noreply.github.com>
2025-08-13 13:04:41 +0800

    Reversesstringupdate

--- ReverseString.java
+++ ReverseString.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
+
 public class ReverseString {
 
+    
+
+
     public static void main(String[] args) {
-        String input = "justen";
-        String reversed = new StringBuilder(input).reverse().toString();
-        System.out.println(reversed);
+
+
+        while (true) {
+            System.out.print("Input your name or 'EXIT' to end: ");
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("EXIT")){
+                System.out.println("Thankyou User!");
+                break;
+            }
+
+
+
+            
+            String reversed = new StringBuilder(input).reverse().toString();
+            System.out.println(reversed);
+
+            
+        }
+
+    
+       
+
+  
+
+
+
     }
 
 }
